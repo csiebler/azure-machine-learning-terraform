d75a5a2592412361982fca26fd96d42516adb2fa
Clemens Siebler <clemens.siebler@gmail.com>
2020-11-17 14:42:36 +0100

    Added DNS zones to Storage Account

--- storage_account.tf
+++ storage_account.tf
@@ -8,6 +8,8 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = "LRS"
 }
 
+# Virtual Network & Firewall configuration
+
 resource "azurerm_storage_account_network_rules" "firewall_rules" {
   resource_group_name  = azurerm_resource_group.example.name
   storage_account_name = azurerm_storage_account.example.name
@@ -17,6 +19,36 @@ resource "azurerm_storage_account_network_rules" "firewall_rules" {
   virtual_network_subnet_ids = [azurerm_subnet.example.id]
   bypass                     = ["AzureServices"]
 }
+
+# DNS Zones
+
+resource "azurerm_private_dns_zone" "sa_zone_blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_private_dns_zone" "sa_zone_file" {
+  name                = "privatelink.file.core.windows.net"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Linking of DNS zones to Virtual Network
+
+resource "azurerm_private_dns_zone_virtual_network_link" "sa_zone_blob_link" {
+  name                  = "${random_string.postfix.result}_link_blob"
+  resource_group_name   = azurerm_resource_group.example.name
+  private_dns_zone_name = azurerm_private_dns_zone.sa_zone_blob.name
+  virtual_network_id    = azurerm_virtual_network.example.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "sa_zone_file_link" {
+  name                  = "${random_string.postfix.result}_link_file"
+  resource_group_name   = azurerm_resource_group.example.name
+  private_dns_zone_name = azurerm_private_dns_zone.sa_zone_file.name
+  virtual_network_id    = azurerm_virtual_network.example.id
+}
+
+# Private Endpoint configuration
 
 resource "azurerm_private_endpoint" "sa_pe_blob" {
   name                = "${var.prefix}-sa-pe-blob-${random_string.postfix.result}"
@@ -29,6 +61,11 @@ resource "azurerm_private_endpoint" "sa_pe_blob" {
     private_connection_resource_id = azurerm_storage_account.example.id
     subresource_names              = ["blob"]
     is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group-blob"
+    private_dns_zone_ids = [azurerm_private_dns_zone.sa_zone_blob.id]
   }
 }
 
@@ -43,5 +80,10 @@ resource "azurerm_private_endpoint" "sa_pe_file" {
     private_connection_resource_id = azurerm_storage_account.example.id
     subresource_names              = ["file"]
     is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group-file"
+    private_dns_zone_ids = [azurerm_private_dns_zone.sa_zone_file.id]
   }
 }
