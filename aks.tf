f1869d80428a11c20a39e7417f8b9f2f7edbe011
Ignacio Floristan <38979090+ignaciofls@users.noreply.github.com>
2020-11-22 18:15:26 +0400

    Update aks.tf
    added depends_on to make sure the AML workspace is created before the attach AKS command 
    network profile added to inject AKS into a subnet
    local-exec provisioner to launch command with az cli

--- aks.tf
+++ aks.tf
@@ -1,6 +1,7 @@
 # Azure Kubernetes Service
 
 resource "azurerm_kubernetes_cluster" "example" {
+  count               = var.deploy_aks ? 1 : 0
   name                = "${var.prefix}-aks-${random_string.postfix.result}"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
@@ -10,10 +11,23 @@ resource "azurerm_kubernetes_cluster" "example" {
     name       = "default"
     node_count = 1
     vm_size    = "Standard_D2_v2"
+	  vnet_subnet_id = azurerm_subnet.example2.id
   }
   
   identity {
     type = "SystemAssigned"
   }
   
+  network_profile {
+    network_plugin = "azure"
+    dns_service_ip = "10.0.3.10"
+    service_cidr = "10.0.3.0/24"
+	docker_bridge_cidr = "172.17.0.1/16"
+  }  
+  
+  provisioner "local-exec" {
+    command = "az ml computetarget attach aks -n myaks -i ${azurerm_kubernetes_cluster.example[count.index].id} -g ${var.resource_group} -w ${azurerm_machine_learning_workspace.example.name}"
+  }
+  
+  depends_on = [azurerm_machine_learning_workspace.example]
 }
